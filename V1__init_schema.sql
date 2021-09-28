57b3ac80ba5a7e26ea8fa5371bada1d2aa1b1229
moseikin <moseikinda@gmail.com>
2021-09-28 12:13:20 +0300

    hope-all-ok

---
+++ V1__init_schema.sql
@@ -0,0 +1,37 @@
+CREATE TABLE School (
+	id INTEGER PRIMARY KEY,
+	name VARCHAR(50) NULL,
+	address VARCHAR (100) NULL);
+	
+CREATE TABLE Teacher (
+	id INTEGER PRIMARY KEY,
+	school_id INTEGER,
+	firstname VARCHAR(50) NULL,
+	secondname VARCHAR(50) NULL,
+	patronymic VARCHAR(50) NULL,
+	age INTEGER,
+	gender VARCHAR(15) NULL,
+	
+	FOREIGN KEY (school_id) REFERENCES School (id));
+
+CREATE TABLE Student (
+     id INTEGER PRIMARY KEY,
+     school_id INTEGER,
+     firstname VARCHAR(50) NULL,
+     secondname VARCHAR(50) NULL,
+     patronymic VARCHAR(50) NULL,
+     age INTEGER,
+     gender VARCHAR(15) NULL,
+
+     FOREIGN KEY (school_id) REFERENCES School(id));
+	
+CREATE TABLE Subject (
+	id INTEGER PRIMARY KEY,
+	teacher_id INTEGER,
+	student_id INTEGER,
+	title VARCHAR(100) NULL,
+	
+	FOREIGN KEY (teacher_id) REFERENCES Teacher(id),
+	FOREIGN KEY (student_id) REFERENCES Student(id));
+	
+
